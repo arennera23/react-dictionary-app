367d8d85b86b1345552ab1b4a1c2f046fb0cde8c
arennera23 <88583364+arennera23@users.noreply.github.com>
2022-06-18 00:38:58 +0900

    Search Engine created

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,16 +1,27 @@
-import React from "react";
 import "./Dictionary.css";
 import { useState } from "react";
 export default function Dictionary() {
+  let [word, setWord] = useState("");
+
+  function handleInput(event) {
+    setWord(event.target.value);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for `);
+    alert(`Searching for ${word}`);
   }
 
   return (
-    <div>
+    <div className="Dictionary">
+      <h1>Dictionary</h1>
       <form onSubmit={search}>
-        <input type="search" className="form-control" />{" "}
+        <input
+          type="search"
+          className="form-control"
+          placeholder="Search for a word..."
+          onChange={handleInput}
+        />{" "}
       </form>
     </div>
   );
